d508d943bfa7459b36a9726805335ab2c9914f16
dja12 <dja12@DESKTOP-TFJ6PKC>
2017-08-17 23:38:23 +0900

    test

--- src/kr/dja/plciot/PLC_IoT_Core.java
+++ src/kr/dja/plciot/PLC_IoT_Core.java
@@ -24,7 +24,7 @@ public class PLC_IoT_Core implements IDependencyTask
 	public final WebServer webServer;
 	
 	private PLC_IoT_Core()
-	{//TEST
+	{//TESTa
 		this.console = new Console();
 		this.mainFrame = new MainFrame(this.console);
 		this.dbManager = new DatabaseConnector(this.console);
