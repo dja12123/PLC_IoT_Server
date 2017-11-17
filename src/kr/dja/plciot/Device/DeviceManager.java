f3391af25a08d8336cf3e9a3a15109728518ea51
엄선용 <@dja-univ-main>
2017-11-17 20:43:15 +0900

    장치 이벤트

--- src/kr/dja/plciot/Device/DeviceManager.java
+++ src/kr/dja/plciot/Device/DeviceManager.java
@@ -65,8 +65,8 @@ public class DeviceManager implements IDeviceView, INewConnectionHandler, IPacke
 		if(observerList == null) return;
 		for(IDeviceEventObserver observer : observerList)
 		{
-			System.out.println("장치 이벤트 알림");
 			observer.deviceEvent(device, key, data);
+			System.out.println("장치 이벤트 알림 완료");
 		}
 	}
 
